69f6272ae20c6c4a8bf7cab5dfc95937923f4d49
RichetinB <baptisterichetin@gmail.com>
2024-05-24 20:03:04 +0200

    ajout first page

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,6 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("styles/main.css");
+    eleventyConfig.addPassthroughCopy("scripts/main.js");
+    eleventyConfig.addPassthroughCopy("scripts/swup.js");
+    eleventyConfig.addPassthroughCopy("assets");
   };

060dde7b033631f17a7b42aee27b2cbb0259ae37
RichetinB <baptisterichetin@gmail.com>
2024-05-28 18:51:14 +0200

    page cv

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("styles/main.css");
+    eleventyConfig.addPassthroughCopy("styles");
     eleventyConfig.addPassthroughCopy("scripts/main.js");
     eleventyConfig.addPassthroughCopy("scripts/swup.js");
     eleventyConfig.addPassthroughCopy("assets");
+    eleventyConfig.addPassthroughCopy("templates");
   };
